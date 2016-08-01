31d5d3ddb50ecbba9ee074ef5afd18ffc66581d8
huangzhi29 <huangzhi29@vip.qq.com>
2016-08-01 16:17:32 +0800

    first commit

---
+++ target/generated-sources/annotations/com/javferna/packtpub/mastering/numericalSummarization/benchmark/generated/ConcurrentBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package com.javferna.packtpub.mastering.numericalSummarization.benchmark.generated;
+public class ConcurrentBenchmark_jmhType extends ConcurrentBenchmark_jmhType_B3 {
+}
+
---
+++ target/generated-sources/annotations/com/javferna/packtpub/mastering/numericalSummarization/benchmark/generated/SerialBenchmark_jmhType.java
@@ -0,0 +1,4 @@
+package com.javferna.packtpub.mastering.numericalSummarization.benchmark.generated;
+public class SerialBenchmark_jmhType extends SerialBenchmark_jmhType_B3 {
+}
+
